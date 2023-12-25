7eeaf9f60a4d4621249ae4b01c200a7b0488152e
MoussaS <MoussaS@DEV-MOUSSAS>
2023-12-25 17:01:36 +0200

    edit get all cards

--- WebScraperApi/WebScraper.cs
+++ WebScraperApi/WebScraper.cs
@@ -9,6 +9,7 @@ namespace WebScraperApi;
 public class WebScraper : ICarterModule
 {
     private IDictionary<string, object> DetailsDict = new Dictionary<string, object>();
+    private List<List<string>> DetailsList = new List<List<string>>();
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         var group = app.MapGroup("/api/webscraper");
@@ -48,37 +49,139 @@ public class WebScraper : ICarterModule
         }
         return "";
     }
+    #region old selnum
+    //private string GetDataWithSelenium(string url)
+    //{
+    //    var chromeOptions = new ChromeOptions() { AcceptInsecureCertificates = true };
+    //    chromeOptions.AddArguments("--headless=new"); // comment out for testing
+    //    var driver = new ChromeDriver(chromeOptions);
+
+    //    // scraping logic...
+    //    driver.Navigate().GoToUrl(url);
+    //    //var html = driver.PageSource;
+    //    //Console.WriteLine(html);
+    //    var Cards = driver.FindElements(By.XPath("//div[@id='cardsresult']//div[@class='row justify-content-center']//div[@class='col-12 col-md-12 mb-4']"));
+    //    foreach (var card in Cards)
+    //    {
+    //        var details_btns = card.FindElements(By.XPath("//div[@class='tender-card rounded card mt-0 mb-0']//div[@class='row']//div[@class='col-12 col-md-9 p-0']//div[@class='tender-metadata border-left border-bottom']//div[@class='row']//div[@class='col-12 border-bottom']//div[@class='row']//div[@class='col-12']//div[@class='pb-2']//div[@class='pull-right']"));
+    //        var details_btn = card.FindElement(By.ClassName("pull-right"));
+    //        var cardOuterHtml = card.GetAttribute("outerHTML");
+
+    //        details_btn.Click();
+
+    //        GetDetails(driver);
+    //        //  var cardOuterHtml = card.GetAttribute("outerHTML");
+    //        driver.Navigate().Back();
+    //        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+    //        wait.Until(driver => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
+    //        Cards = driver.FindElements(By.XPath("//div[@id='cardsresult']//div[@class='row justify-content-center']//div[@class='col-12 col-md-12 mb-4']"));
+
+    //        // Find the card element again using its outer HTML
+    //        var refreshedCard = Cards.FirstOrDefault(c => c.GetAttribute("outerHTML") == cardOuterHtml);
+
+    //        if (refreshedCard != null)
+    //        {
+    //            // Perform actions on the refreshed card, e.g., click details button again
+    //            var refreshedDetailsBtn = refreshedCard.FindElement(By.ClassName("pull-right"));
+    //            refreshedDetailsBtn.Click();
+    //            GetDetails(driver);
+    //            driver.Navigate().Back();
+    //            //   WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+    //            wait.Until(driver => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
+    //        }
+    //        else
+    //        {
+    //            // Handle the case where the card is not found on the refreshed page
+    //            Console.WriteLine("Card not found on the refreshed page.");
+    //        }
+
+    //    }
+    //    //  GetPagination(driver);
+    //    // close the browser and release its resources
+    //    driver.Quit();
+    //    return "";
+    //}
+    //private void GetDetails(ChromeDriver driver)
+    //{
+    //    List<string> card = new List<string>();
+    //    var details = driver.FindElements(By.ClassName("form-details-list"));
+    //    foreach (var detail in details)
+    //    {
+    //        var items = detail.FindElements(By.ClassName("list-group-item"));
+    //        foreach (var item in items)
+    //        {
+    //            var title = item.FindElement(By.ClassName("etd-item-title")).Text;
+    //            var infoSpans = item.FindElements(By.ClassName("etd-item-info"));
+    //            string info = string.Empty;
+    //            if (infoSpans.Any())
+    //            {
+    //                foreach (var infoSpan in infoSpans)
+    //                {
+    //                    info += infoSpan.Text;
+    //                }
+    //                //todo add details to database and change the return of the function to void
+    //            }
+    //            card.Add(info);
+    //            // DetailsDict.Add(title, info);
+    //        }
+    //        DetailsList.Add(card);
+    //    }
+
+    //}
+
+    #endregion
+
+
+    #region new
     private string GetDataWithSelenium(string url)
     {
         var chromeOptions = new ChromeOptions() { AcceptInsecureCertificates = true };
         chromeOptions.AddArguments("--headless=new"); // comment out for testing
-        var driver = new ChromeDriver(chromeOptions);
 
-        // scraping logic...
-        driver.Navigate().GoToUrl(url);
-        //var html = driver.PageSource;
-        //Console.WriteLine(html);
-        var Cards = driver.FindElements(By.XPath("//div[@id='cardsresult']//div[@class='row justify-content-center']//div[@class='col-12 col-md-12 mb-4']"));
-        foreach (var card in Cards)
+        using (var driver = new ChromeDriver(chromeOptions))
         {
-            //var details_btns = card.FindElements(By.XPath("//div[@class='tender-card rounded card mt-0 mb-0']//div[@class='row']//div[@class='col-12 col-md-9 p-0']//div[@class='tender-metadata border-left border-bottom']//div[@class='row']//div[@class='col-12 border-bottom']//div[@class='row']//div[@class='col-12']//div[@class='pb-2']//div[@class='pull-right']"));
-            var details_btn = card.FindElement(By.ClassName("pull-right"));
+            driver.Navigate().GoToUrl(url);
 
-            details_btn.Click();
+            var cards = driver.FindElements(By.XPath("//div[@id='cardsresult']//div[@class='row justify-content-center']//div[@class='col-12 col-md-12 mb-4']"));
 
-            GetDetails(driver);
-            driver.Navigate().Back();
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            wait.Until(driver => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
-            Cards = driver.FindElements(By.XPath("//div[@id='cardsresult']//div[@class='row justify-content-center']//div[@class='col-12 col-md-12 mb-4']"));
+            for (int i = 0; i < cards.Count; i++)
+            {
+                // Find elements on each iteration to avoid staleness
+                var card = driver.FindElements(By.XPath("//div[@id='cardsresult']//div[@class='row justify-content-center']//div[@class='col-12 col-md-12 mb-4']"))[i];
+                var detailsBtn = card.FindElement(By.ClassName("pull-right"));
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(50));
+                wait.Until(driver => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", detailsBtn);
+
+                detailsBtn.Click();
+                GetDetails(driver, card);
+                driver.Navigate().Back();
+                // Find elements again after the back navigation
+                cards = driver.FindElements(By.XPath("//div[@id='cardsresult']//div[@class='row justify-content-center']//div[@class='col-12 col-md-12 mb-4']"));
+            }
+
+            driver.Quit();
         }
-        GetPagination(driver);
-        // close the browser and release its resources
-        driver.Quit();
+
         return "";
     }
-    private void GetDetails(ChromeDriver driver)
+
+    // Modify GetDetails to accept a card element and extract information
+    //private void GetDetails(IWebDriver driver, IWebElement card)
+    //{
+    //    // Implement your logic to extract details from the card element
+    //    // For example:
+    //    var title = card.FindElement(By.XPath(".//div[@class='tender-card-title']")).Text;
+    //    var description = card.FindElement(By.XPath(".//div[@class='tender-card-description']")).Text;
+
+    //    // Print or store the extracted details as needed
+    //    Console.WriteLine($"Title: {title}");
+    //    Console.WriteLine($"Description: {description}");
+    //    Console.WriteLine("--------------");
+    //}
+    private void GetDetails(IWebDriver driver, IWebElement card)
     {
+        List<string> carddetails = new List<string>();
         var details = driver.FindElements(By.ClassName("form-details-list"));
         foreach (var detail in details)
         {
@@ -96,11 +199,15 @@ public class WebScraper : ICarterModule
                     }
                     //todo add details to database and change the return of the function to void
                 }
-                DetailsDict.Add(title, info);
+                carddetails.Add(info);
+                // DetailsDict.Add(title, info);
             }
+            DetailsList.Add(carddetails);
         }
-
     }
+    #endregion
+
+
     private void GetPagination(ChromeDriver driver)
     {
         var page = driver.FindElements(By.ClassName("pagination-primary"))[0];
