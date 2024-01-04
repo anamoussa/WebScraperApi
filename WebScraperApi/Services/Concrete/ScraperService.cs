1d609f7f00284b703d728552b23105de344cdab3
Mouss <Mouss@DESKTOP-EHP15EQ>
2024-01-04 08:00:27 +0200

    aysnc

--- WebScraperApi/Services/Concrete/ScraperService.cs
+++ WebScraperApi/Services/Concrete/ScraperService.cs
@@ -1,4 +1,10 @@
-﻿namespace WebScraperApi.Services.Concrete;
+﻿using OpenQA.Selenium.Support.UI;
+using static OpenQA.Selenium.VirtualAuth.VirtualAuthenticatorOptions;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System;
+
+namespace WebScraperApi.Services.Concrete;
 
 public class ScraperService : IScraperService
 {
@@ -17,11 +23,11 @@ public class ScraperService : IScraperService
     }
     public async Task GetDataAsync()
     {
-        for (int i = 1; i < 2562; i++)
+        for (int i = 6; i < 2562; i++)
         {
             await Console.Out.WriteLineAsync("*********page Number  " + i + "  **********************");
             var CardsBasicDataFromDB = await _service.GetCardsIDsPagesFromDBAsync(100, i);
-            GetAllRelatedData(CardsBasicDataFromDB.ToList());
+           await GetAllRelatedData(CardsBasicDataFromDB.ToList());
 
         }
 
@@ -30,145 +36,130 @@ public class ScraperService : IScraperService
         //pass list of ids to getall relateddata
         return;
     }
-    private void GetAllRelatedData(List<string> CardsIDs)
+    private async Task GetAllRelatedData(List<string> CardsIDs)
     {
-        int counter = 0;
         foreach (var tenderID in CardsIDs)
         {
-
-            GetDetailsForVisitor(tenderID);
-            GetTenderDates(tenderID);
-            GetAwardingResult(tenderID);
-            GetRelationsDetails(tenderID);
-
-            if (counter == 10)
-            {
-                //var recordsToDelete = _context.CardBasicDatas
-                //.Where(c => tenderIDs.Contains(c.tenderIdString))
-                //.ToList();
-                //_context.CardBasicDatas.RemoveRange(recordsToDelete);
-                //_context.SaveChanges();
-                // _context.CardBasicDatas.AddRange(CardsBasicData);
-
-                _context.GetTenderDates.AddRange(GetTenderDatesList);
-                _context.GetRelationsDetails.AddRange(GetRelationsDetailsList);
-                _context.GetAwardingResults.AddRange(GetAwardingResultsList);
-                _context.GetDetailsForVisitor.AddRange(DetailsForVisitorsList);
-                _context.SaveChanges();
-                DetailsForVisitorsList = new();
-                GetTenderDatesList = new();
-                GetAwardingResultsList = new();
-                GetRelationsDetailsList = new();
-                counter = 0;
-            }
-            counter++;
+         //  await GetTenderDates(tenderID);
+           await GetAwardingResult(tenderID);
+            //GetDetailsForVisitor(tenderID);
+            //GetRelationsDetails(tenderID);
             Console.WriteLine("_______________________******_____" + CardsIDs.IndexOf(tenderID));
         }
+      //  _context.GetTenderDates.AddRange(GetTenderDatesList);
+        _context.GetAwardingResults.AddRange(GetAwardingResultsList);
+        //_context.GetRelationsDetails.AddRange(GetRelationsDetailsList);
+        //_context.GetDetailsForVisitor.AddRange(DetailsForVisitorsList);
+        _context.SaveChanges();
+     //   GetTenderDatesList = new();
+        GetAwardingResultsList = new();
+        //DetailsForVisitorsList = new();
+        //GetRelationsDetailsList = new();
     }
-    private void GetTenderDates(string tenderID)
+    private async Task GetTenderDates(string tenderID)
     {
-        var url = Constants.UrlGetTenderDates + tenderID;
-        var chromeOptions = new ChromeOptions() { AcceptInsecureCertificates = true };
-        chromeOptions.AddArguments("--headless=new"); // comment out for testing
-        using var driver = new ChromeDriver(chromeOptions);
-        driver.Manage().Timeouts().PageLoad.Add(TimeSpan.FromMinutes(5));
-        driver.Manage().Timeouts().ImplicitWait.Add(TimeSpan.FromMinutes(5));
-        driver.Manage().Timeouts().AsynchronousJavaScript.Add(TimeSpan.FromMinutes(5));
-        driver.Navigate().GoToUrl(url);
-        GetTenderDate tenderDates = new GetTenderDate();
-        tenderDates.tenderIdString = tenderID;
-        var dates = driver.FindElements(By.CssSelector("li.list-group-item"));
-        foreach (var date in dates)
+        await Task.Run(() =>
         {
-            var title = date.FindElement(By.ClassName("etd-item-title"));
-            var info = date.FindElement(By.ClassName("etd-item-info"));
-            var txt = title.Text;
-            var span = info.FindElements(By.TagName("span")).Select(o => o.Text).ToArray();
-            try
+            GetTenderDate tenderDates = new GetTenderDate();
+            var url = Constants.UrlGetTenderDates + tenderID;
+            var chromeOptions = new ChromeOptions() { AcceptInsecureCertificates = true };
+            chromeOptions.AddArguments("--headless=new"); // comment out for testing
+            using var driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl(url);
+            // WebDriverWait webDriverWait = new WebDriverWait(driver, TimeSpan.FromMinutes(2));
+            tenderDates.tenderIdString = tenderID;
+            var dates = driver.FindElements(By.CssSelector("li.list-group-item"));
+            foreach (var date in dates)
             {
-                switch (txt)
+                var title = date.FindElement(By.ClassName("etd-item-title"));
+                var info = date.FindElement(By.ClassName("etd-item-info"));
+                var txt = title.Text;
+                var span = info.FindElements(By.TagName("span")).Select(o => o.Text).ToArray();
+                try
                 {
-                    #region un used Data
-                    //// dublicated
-                    //case "اخر موعد لإستلام الاستفسارات":
-                    //    tenderDates.lastEnqueriesDate = span[0] == "لا يوجد" ? null : DateOnly.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
-                    //    tenderDates.lastEnqueriesDateHijri = span[1];
-                    //    break;
-                    //// dublicated
-                    //case "آخر موعد لتقديم العروض":
-                    //    tenderDates.lastOfferPresentationDate = span[0] == "لا يوجد" ? null : DateOnly.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
-                    //    tenderDates.lastOfferPresentationDateHijri = span[1];
-                    //    tenderDates.lastOfferPresentationTime = span[2] == "لا يوجد" ? null : TimeOnly.ParseExact(span[2], "h:mm tt", CultureInfo.InvariantCulture);
-                    //    break;
-                    //// dublicated
-                    //case "تاريخ فتح العروض":
-                    //    tenderDates.offersOpeningDate = span[0] == "لا يوجد" ? null : DateTime.Parse(span[0]);
-                    //    break;
-                    //case "تاريخ فحص العروض":
-                    //    if (span[0] == "لا يوجد")
-                    //        break;
-                    //    tenderDates.offersExaminationDate = span[0] == "لا يوجد" ? null : DateOnly.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
-                    //    tenderDates.offersExaminationDateHijri = span[1];
-                    //    tenderDates.offersExaminationTime = span[2] == "لا يوجد" ? null : TimeOnly.ParseExact(span[2], "h:mm tt", CultureInfo.InvariantCulture);
-                    //    break; 
-                    #endregion
-                    case "التاريخ المتوقع للترسية":
-                        if (span[0] == "لا يوجد")
+                    switch (txt)
+                    {
+                        #region un used Data
+                        //// dublicated
+                        //case "اخر موعد لإستلام الاستفسارات":
+                        //    tenderDates.lastEnqueriesDate = span[0] == "لا يوجد" ? null : DateOnly.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                        //    tenderDates.lastEnqueriesDateHijri = span[1];
+                        //    break;
+                        //// dublicated
+                        //case "آخر موعد لتقديم العروض":
+                        //    tenderDates.lastOfferPresentationDate = span[0] == "لا يوجد" ? null : DateOnly.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                        //    tenderDates.lastOfferPresentationDateHijri = span[1];
+                        //    tenderDates.lastOfferPresentationTime = span[2] == "لا يوجد" ? null : TimeOnly.ParseExact(span[2], "h:mm tt", CultureInfo.InvariantCulture);
+                        //    break;
+                        //// dublicated
+                        //case "تاريخ فتح العروض":
+                        //    tenderDates.offersOpeningDate = span[0] == "لا يوجد" ? null : DateTime.Parse(span[0]);
+                        //    break;
+                        //case "تاريخ فحص العروض":
+                        //    if (span[0] == "لا يوجد")
+                        //        break;
+                        //    tenderDates.offersExaminationDate = span[0] == "لا يوجد" ? null : DateOnly.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                        //    tenderDates.offersExaminationDateHijri = span[1];
+                        //    tenderDates.offersExaminationTime = span[2] == "لا يوجد" ? null : TimeOnly.ParseExact(span[2], "h:mm tt", CultureInfo.InvariantCulture);
+                        //    break; 
+                        #endregion
+                        case "التاريخ المتوقع للترسية":
+                            if (span[0] == "لا يوجد")
+                                break;
+                            tenderDates.expectedAwardDate = span[0] == "لا يوجد" ? null : DateTime.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                            tenderDates.expectedAwardDateHijri = span[1];
                             break;
-                        tenderDates.expectedAwardDate = span[0] == "لا يوجد" ? null : DateTime.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
-                        tenderDates.expectedAwardDateHijri = span[1];
-                        break;
-                    case "تاريخ بدء الأعمال / الخدمات":
-                        if (span[0] == "لا يوجد")
+                        case "تاريخ بدء الأعمال / الخدمات":
+                            if (span[0] == "لا يوجد")
+                                break;
+                            tenderDates.businessStartDate = span[0] == "لا يوجد" ? null : DateTime.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                            tenderDates.businessStartDateHijri = span[1];
                             break;
-                        tenderDates.businessStartDate = span[0] == "لا يوجد" ? null : DateTime.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
-                        tenderDates.businessStartDateHijri = span[1];
-                        break;
-                    case "تاريخ استحقاق خطاب تأكيد المشاركة":
-                        tenderDates.participationConfirmationLetterDate = span[0] == "لا يوجد" ? null : DateTime.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture); ;
-                        break;
-                    case "بداية إرسال الأسئلة و الاستفسارات":
-                        if (span[0] == "لا يوجد")
+                        case "تاريخ استحقاق خطاب تأكيد المشاركة":
+                            tenderDates.participationConfirmationLetterDate = span[0] == "لا يوجد" ? null : DateTime.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture); ;
                             break;
-                        tenderDates.sendingInquiriesDate = span[0] == "لا يوجد" ? null : DateTime.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
-                        tenderDates.sendingInquiriesDateHijri = span[1];
-                        break;
-                    case "أقصى مدة للإجابة على الإستفسارات(أيام)":
-                        tenderDates.AnswerInquiriesInDays = int.Parse(span[0]);
-                        break;
-                    case "مكان فتح العرض":
-                        tenderDates.offersOpeningLocation = span[0];
-                        break;
+                        case "بداية إرسال الأسئلة و الاستفسارات":
+                            if (span[0] == "لا يوجد")
+                                break;
+                            tenderDates.sendingInquiriesDate = span[0] == "لا يوجد" ? null : DateTime.ParseExact(span[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                            tenderDates.sendingInquiriesDateHijri = span[1];
+                            break;
+                        case "أقصى مدة للإجابة على الإستفسارات(أيام)":
+                            tenderDates.AnswerInquiriesInDays = int.Parse(span[0]);
+                            break;
+                        case "مكان فتح العرض":
+                            tenderDates.offersOpeningLocation = span[0];
+                            break;
+                    }
+
                 }
+                catch (Exception ex)
+                {
 
+                    Console.WriteLine("Error_________" + ex.Message);
+                }
             }
-            catch (Exception ex)
-            {
+            GetTenderDatesList.Add(tenderDates);
+            driver.Quit();
 
-                Console.WriteLine("Error_________" + ex.Message);
-            }
         }
-        GetTenderDatesList.Add(tenderDates);
-        driver.Quit();
-
+            );
     }
-    private void GetAwardingResult(string tenderID)
+    private async Task  GetAwardingResult(string tenderID)
     {
-        GetAwardingResult awardingResult = new GetAwardingResult();
-        awardingResult.tenderIdString = tenderID;
-        var url = Constants.UrlGetAwardingResult + tenderID;
-        var chromeOptions = new ChromeOptions() { AcceptInsecureCertificates = true };
-        chromeOptions.AddArguments("--headless=new"); // comment out for testing
-        using (var driver = new ChromeDriver(chromeOptions))
+       await Task.Run(() =>
         {
-
+            GetAwardingResult awardingResult = new GetAwardingResult();
             OfferApplicant offerApplicant = null;
             List<OfferApplicant> offerApplicants = new List<OfferApplicant>();
-
             AwardedSupplier awardedSupplier = null;
             List<AwardedSupplier> awardedSuppliers = new List<AwardedSupplier>();
+            awardingResult.tenderIdString = tenderID;
+            var url = Constants.UrlGetAwardingResult + tenderID;
+            var chromeOptions = new ChromeOptions() { AcceptInsecureCertificates = true };
+            chromeOptions.AddArguments("--headless=new"); // comment out for testing
+            using var driver = new ChromeDriver(chromeOptions);
             driver.Navigate().GoToUrl(url);
-
             try
             {
                 IWebElement element2 = driver.FindElement(By.ClassName("card-body"));
@@ -210,8 +201,10 @@ public class ScraperService : IScraperService
             }
             awardingResult.awardedSuppliers = awardedSuppliers;
             awardingResult.offerApplicants = offerApplicants;
-        }
-        GetAwardingResultsList.Add(awardingResult);
+
+            GetAwardingResultsList.Add(awardingResult);
+        });
+        
     }
     private void GetRelationsDetails(string tenderID)
     {
@@ -315,7 +308,6 @@ public class ScraperService : IScraperService
         ChromeOptions options = new ChromeOptions();
         options.AddArgument("--headless"); // Run Chrome in headless mode (without opening a browser window)
         IWebDriver driver = new ChromeDriver(options);
-
         // Navigate to the page
         driver.Navigate().GoToUrl(url);
 
@@ -404,3 +396,31 @@ public class ScraperService : IScraperService
         DetailsForVisitorsList.Add(detailsForVisitor);
     }
 }
+//OpenQA.Selenium.WebDriverException
+//  HResult = 0x80131500
+//  Message=The HTTP request to the remote WebDriver server for URL http://localhost:6664/session timed out after 60 seconds.
+//  Source=WebDriver
+//  StackTrace:
+//   at OpenQA.Selenium.Remote.HttpCommandExecutor.Execute(Command commandToExecute)
+//   at OpenQA.Selenium.Remote.DriverServiceCommandExecutor.Execute(Command commandToExecute)
+//   at OpenQA.Selenium.WebDriver.Execute(String driverCommandToExecute, Dictionary`2 parameters)
+//   at OpenQA.Selenium.WebDriver.StartSession(ICapabilities desiredCapabilities)
+//   at OpenQA.Selenium.WebDriver..ctor(ICommandExecutor executor, ICapabilities capabilities)
+//   at OpenQA.Selenium.Chrome.ChromeDriver..ctor(ChromeOptions options)
+//   at WebScraperApi.Services.Concrete.ScraperService.<>c__DisplayClass10_0.<GetAwardingResult>b__0() in C:\Users\Mouss\OneDrive\Desktop\Scraper\v1\WebScraperApi\WebScraperApi\Services\Concrete\ScraperService.cs:line 157
+//   at System.Threading.ExecutionContext.RunFromThreadPoolDispatchLoop(Thread threadPoolThread, ExecutionContext executionContext, ContextCallback callback, Object state)
+
+//Inner Exception 1:
+//TaskCanceledException: The request was canceled due to the configured HttpClient.Timeout of 60 seconds elapsing.
+
+//Inner Exception 2:
+//TimeoutException: The operation was canceled.
+
+//Inner Exception 3:
+//TaskCanceledException: The operation was canceled.
+
+//Inner Exception 4:
+//IOException: Unable to read data from the transport connection: The I/O operation has been aborted because of either a thread exit or an application request..
+
+//Inner Exception 5:
+//SocketException: The I/O operation has been aborted because of either a thread exit or an application request.
